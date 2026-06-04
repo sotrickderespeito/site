ead7c43b304219d63fad40b1109fb230ba6a35d7
SóTrickDeRespeito <sotrickderespeito@gmail.com>
2026-06-04 02:22:58 -0300

    fix: resolve __dirname issue in vite config
    Define __dirname manually using fileURLToPath to support ESM environment requirements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
